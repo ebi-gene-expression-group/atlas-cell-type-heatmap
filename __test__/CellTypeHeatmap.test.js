40a2b7dae9781f724c7ff467295a28d34fea57f3
lingyun1010 <zhaolingyun1010@gmail.com>
2019-11-05 16:51:38 +0000

    Add tests and snapshots for components

---
+++ __test__/CellTypeHeatmap.test.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Enzyme from 'enzyme'
+import {shallow, mount} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import renderer from 'react-test-renderer'
+
+import '@babel/polyfill'
+import fetchMock from 'fetch-mock'
+
+import CalloutAlert from '../src/CalloutAlert'
+import CellTypeHeatmap from '../src/CellTypeHeatmap'
+import HighchartsHeatmap from '../src/HighchartsHeatmap'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe(`CellTypeHeatmap`, () => {
+  beforeEach(() => {
+    fetchMock.restore()
+  })
+
+  const props = {
+    host: `foo/`,
+    resource: `bar`
+  }
+
+  const validProps = {
+    host: 'http://localhost:8080/gxa/sc/',
+    resource: 'json/experiments/celltype/name/organism_part/value/pancreas'
+  }
+
+  test(`Renders error if API request is unsuccessful`, () => {
+    const wrapper = shallow(<CellTypeHeatmap {...props} />)
+    expect(wrapper.exists(CalloutAlert)).toBe(true)
+  })
+
+  test(`Renders heatmap if API request is successful`, () => {
+    const wrapper = mount(<CellTypeHeatmap {...validProps} />)
+
+    Promise
+      .resolve(wrapper)
+      .then(() => wrapper.update())
+      .then(() => wrapper.update())
+      .then(() => {
+        expect(wrapper.exists(HighchartsHeatmap)).toBe(true)
+      })
+  })
+
+  it(`matches snapshot`, () => {
+    const tree = renderer.create(<CellTypeHeatmap {...validProps} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
